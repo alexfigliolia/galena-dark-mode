eeff91a66349fc2aa64051319170c7de557e741f
Alex Figliolia <alexfigliolia@gmail.com>
2024-08-02 19:31:22 -0400

    Designate subscription types

--- src/DarkModeManager.ts
+++ src/DarkModeManager.ts
@@ -2,29 +2,26 @@ import { State } from "@figliolia/galena";
 import type { ITheme, IThemeName, ThemeChangeEvent } from "./types";
 
 export class DarkModeManager extends State<ITheme> {
-  listener: string;
-  browserSupport?: boolean;
-  matcher?: MediaQueryList;
+  private listener?: string;
+  private matcher?: MediaQueryList;
   private onThemeChange?: ThemeChangeEvent;
   constructor(onThemeChange?: ThemeChangeEvent) {
     super("Theme", { theme: "light" });
-    this.onThemeChange = onThemeChange;
     this.updateDocument("light");
-    this.listener = this.subscribe(this.internalUpdate);
+    this.onThemeChange = onThemeChange;
+    this.subscribeInternal();
   }
 
   public initialize() {
-    this.browserSupport = !!window.matchMedia;
-    this.matcher = window.matchMedia("(prefers-color-scheme: dark)");
-    if (this.browserSupport && this.matcher.matches) {
-      this.set("dark");
-    }
-    this.matcher.addEventListener("change", this.themeChange);
+    this.subscribeInternal();
+    this.subscribeOS();
   }
 
   public destroy() {
-    this.matcher?.removeEventListener?.("change", this.themeChange);
-    this.unsubscribe(this.listener);
+    this.matcher?.removeEventListener?.("change", this.onOSSettingsChange);
+    if (this.listener) {
+      this.unsubscribe(this.listener);
+    }
   }
 
   public toggle = () => {
@@ -37,11 +34,28 @@ export class DarkModeManager extends State<ITheme> {
     });
   }
 
-  private themeChange = (e: MediaQueryListEvent) => {
+  private subscribeInternal() {
+    if (!this.listener) {
+      this.listener = this.subscribe(this.onChangeInternal);
+    }
+  }
+
+  private subscribeOS() {
+    this.matcher = window?.matchMedia?.("(prefers-color-scheme: dark)");
+    if (!this.matcher) {
+      return;
+    }
+    if (this.matcher.matches) {
+      this.set("dark");
+    }
+    this.matcher.addEventListener("change", this.onOSSettingsChange);
+  }
+
+  private onOSSettingsChange = (e: MediaQueryListEvent) => {
     this.set(e.matches ? "dark" : "light");
   };
 
-  private internalUpdate = ({ theme }: ITheme) => {
+  private onChangeInternal = ({ theme }: ITheme) => {
     this.updateDocument(theme);
     this.onThemeChange?.(theme);
   };
